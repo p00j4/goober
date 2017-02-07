10f926ed37731de236d0230430e97ade1e027e7f
Pooja Shah <pooja@moengage.com>
2017-02-07 17:26:08 +0530

    extra flag set fix when prefix is not set

--- goober.py
+++ goober.py
@@ -32,21 +32,23 @@ class Goober(Plugin):
                           help="Environment variables to prepend to goober's output. For example, --goober-prefix='LOCALE' will attach 'LOCALE=<os.environ.get('LOCALE')> to 'nosetests -v --goober'")
         parser.add_option('--goober-extra',
                           action='store',
+                          type='string',
+                          dest= 'extra',
                           help="append additional nose options to goober's output (give comma separated). For example: --goober-extra=--with-xunit,--with-xcover,--xcoverage-file=coverage.xml")
 
         super(Goober, self).options(parser, env)
 
     def configure(self, options, conf):
         super(Goober, self).configure(options, conf)
+        self.extra_options = str(options.extra).replace(',', ' ')
         self.prefix = ''
-        self.extra_options = ''
+
         if not options.prefix:
             return
         self.env_vars = options.prefix.split(',')
         if self.env_vars:
             self.assemble_prefix()
-        if options.goober_extra:
-            self.extra_options = str(options.goober_extra).replace(","," ")
+
 
     def assemble_prefix(self):
         self.prefix += ' '.join(["%s=%s" % (var, os.environ.get(var)) for var in self.env_vars if os.environ.get(var) is not None]) + ' '
@@ -98,6 +100,7 @@ class Goober(Plugin):
         msg = "nosetests -v --goober "
         if self.prefix:
             msg = self.prefix + msg + '--goober-prefix=' + ','.join(self.env_vars) + ' '
+
         if self.extra_options:
             msg += str(self.extra_options) + ' '
         
